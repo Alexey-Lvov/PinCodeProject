8f36cc74e07a07afe6faa9e25359ebaa23c48c37
Alexey Lvov <dunice@Mac-mini-dunice-9.local>
2022-09-07 17:39:45 +0300

    fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import bem from 'utils/bem';
-import favicon from 'img/favicon.png';
 import './style.scss';
 
 const b = bem('header');
@@ -9,8 +8,7 @@ function Header() {
   return (
     <div className={b('')}>
       <div className={b('logo')}>
-        <img className={b('favicon')} src={favicon} alt="gif" />
-        <span>LEADER BOARD</span>
+        <span>LOGO</span>
       </div>
     </div>
   );
